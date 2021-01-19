c7151f9c696d779bbf0fc40a3dcdbf813c4887c3
silasstec <77503063+silasstec@users.noreply.github.com>
2021-01-19 16:32:59 +0000

    Update en.php
    changes with english text

--- lang/en.php
+++ lang/en.php
@@ -2,19 +2,19 @@
 
 /** MENU**/
 
-$lang['menu']['sobre'] = 'SOBRE NÓS';
-$lang['menu']['contato'] = 'CONTACTO';
-$lang['menu']['servicos']['titulo'] = 'Serviços';
-$lang['menu']['servicos'][1] = 'ESTRATÉGIA E INOVAÇÃO';
-$lang['menu']['servicos'][2] = 'SUSTENTABILIDADE';
-$lang['menu']['servicos'][3] = 'TECNOLOGIA';
+$lang['menu']['sobre'] = 'ABOUT US';
+$lang['menu']['contato'] = 'CONTACT';
+$lang['menu']['servicos']['titulo'] = 'SERVICES';
+$lang['menu']['servicos'][1] = 'STRATEGY AND INNOVATION';
+$lang['menu']['servicos'][2] = 'SUSTAINABILITY';
+$lang['menu']['servicos'][3] = 'tecnologia';
 
 /** END MENU **/
 
 /** FOOTER **/
 
-$lang['footer']['text'] = '2020 © Consulting Brics. Todos os direitos reservados. Developed By <a href="https://www.silastec.com" style="color:white; font-weight:900;">SilasTec</a>';
-$lang['footer']['legal'] = 'INFORMAÇÃO LEGAL';
+$lang['footer']['text'] = '2020 © Consulting Brics. All rights reserved. Developed By <a href="https://www.silastec.com" style="color:white; font-weight:900;">SilasTec</a>';
+$lang['footer']['legal'] = 'LEGAL INFORMATION';
 
 /** END FOOTER**/
 
@@ -22,174 +22,174 @@ $lang['footer']['legal'] = 'INFORMAÇÃO LEGAL';
 
 $lang['homepage'] = [];
 
-$lang['homepage']['header']['h1'] = 'Somos a ponte entre si e os seus objetivos.';
-$lang['homepage']['header']['button'] = 'CONTACTE-NOS';
+$lang['homepage']['header']['h1'] = 'We are the bridge between you and your goals.';
+$lang['homepage']['header']['button'] = 'CONTACT US';
 
-$lang['homepage']['sobre']['cargo_elenice'] = 'Fundadora & Chefe de Estratégia Internacional';
-$lang['homepage']['sobre']['cargo_antonio'] = 'Diretor Executivo (CEO)';
+$lang['homepage']['sobre']['cargo_elenice'] = 'Founder & Head of International Strategy';
+$lang['homepage']['sobre']['cargo_antonio'] = 'Executive Director (CEO)';
 
-$lang['homepage']['sobre']['h5'] = 'SOBRE NÓS';
-$lang['homepage']['sobre']['h2'] = 'Quem somos?';
+$lang['homepage']['sobre']['h5'] = 'ABOUT US';
+$lang['homepage']['sobre']['h2'] = 'Who are we?';
 
-$lang['homepage']['sobre']['text1'] = "Somos uma consultora focada na migração sustentável.
+$lang['homepage']['sobre']['text1'] = "We are a consultant focused on sustainable migration.
 <br><br>
-No mundo empresarial, os modelos de negócio sustentável são um
-interesse crescente. É nesse sentido que proporcionamos métodos de
-apoio a empresas cujo objetivo passa por se renovarem de forma
-ecologicamente benéfica.";
+In the business world, sustainable business models are a
+growing interest. It is in this sense that we provide methods of
+support to companies whose objective is to renew themselves in a
+ecologically beneficial.";
 
-$lang['homepage']['sobre']['text2'] = "Aliado a isto, é conhecida a importância de uma empresa estar tão bem
-posicionada a nível nacional, como internacional. O crescimento fora do
-mercado brasileiro traz desafios diversos, nem sempre visíveis, que
-conhecemos e compreendemos.
+$lang['homepage']['sobre']['text2'] = "In addition to this, the importance of a company being so well known
+positioned nationally, as internationally. Growth outside the
+Brazilian market brings different challenges, not always visible, that
+we know and understand.
 <br><br>
-Esta visão é, em grande parte, resultante da vasta experiência da nossa fundadora, 
-Elenice Candido da Silva, que se destaca como conhecedora e especialista nas 
-nossas áreas de atuação.
+This vision is largely the result of the vast experience of our founder,
+Elenice Candido da Silva, who stands out as a knowledgeable and expert in
+our areas of expertise.
 <br><br>
-Com um fornecimento de serviços que ajuda os nossos clientes a expandirem-se 
-globalmente, exploramos desafios e conquistamos os seus objetivos.";
+With a supply of services that helps our customers to expand
+globally, we explore challenges and achieve your goals.";
 
-$lang['homepage']['comentario'] = "A Consulting Brics nasceu do sonho de conectar o Brasil ao mundo e dar-lhe 
-a conhecer o seu enorme potencial. Nascida no Mato Grosso e criada na fazenda de família, idealizei e construí 
-uma empresa de consultoria global, com o objetivo de ajudar na urgente migração dos modelos de negócio para 
-as áreas da sustentabilidade.";
-
-
-$lang['homepage']['missao']['titulo'] = 'A nossa missão';
-
-$lang['homepage']['missao']['descricao'] = 'Suportar, através dos nossos serviços, a migração sustentável dos 
-modelos de negócio, para se adaptar às novas exigências do mercado 
-e valorizar o branding atribuindo um maior valor socioambiental.';
-
-$lang['homepage']['visao']['titulo'] = 'A nossa visão';
-
-$lang['homepage']['visao']['descricao'] = 'Criar um ambiente económico global onde empresas e particulares 
-podem potencializar ideias e negócios baseados nos pilares da 
-sustentabilidade.';
+$lang['homepage']['comentario'] = "Consulting Brics was born from the dream of connecting Brazil to the world and giving it
+to know its enormous potential. Born in Mato Grosso and raised on the family farm, I idealized and built
+a global consulting company, with the aim of helping in the urgent migration of business models to
+the areas of sustainability.";
 
 
-$lang['homepage']['valores']['titulo'] = 'Os nossos valores';
+$lang['homepage']['missao']['titulo'] = 'Our mission';
 
-$lang['homepage']['valores']['descricao'] = 'Inovação na procura de soluções;
-Transparência com os clientes;
-Autenticidade na forma de atuar;
-Sofisticação perante os objetivos;
-Confiança nos nossos conhecimentos.';
+$lang['homepage']['missao']['descricao'] = 'Support, through our services, the sustainable migration of
+business models, to adapt to new market demands
+and enhance branding by assigning greater socio-environmental value.';
 
-$lang['homepage']['servicos']['h5'] = 'Serviços';
-$lang['homepage']['servicos']['h2'] = 'O que fazemos?';
-$lang['homepage']['servicos']['item1']['titulo'] = 'estratégia e inovação';
-$lang['homepage']['servicos']['item1']['descricao'] = 'Desenvolvimento de Negócios e Ideias<br>
-Estratégia de Mercado<br>
-Consultoria em Comércio Externo<br>
-Consultoria Legal';
+$lang['homepage']['visao']['titulo'] = 'Our vision';
 
-$lang['homepage']['servicos']['item2']['titulo'] = 'SUSTENTABILIDADE';
-$lang['homepage']['servicos']['item2']['descricao'] = 'Consultoria em Meio Ambiente<br>
+$lang['homepage']['visao']['descricao'] = 'Create a global economic environment where companies and individuals
+can enhance ideas and businesses based on the pillars of
+sustainability.';
+
+
+$lang['homepage']['valores']['titulo'] = 'Our values';
+
+$lang['homepage']['valores']['descricao'] = 'Innovation in the search for solutions;
+Transparency with customers;
+Authenticity in the way of acting;
+Sophistication towards the objectives;
+Trust in our knowledge. ';
+
+$lang['homepage']['servicos']['h5'] = 'Services';
+$lang['homepage']['servicos']['h2'] = 'What do we do?';
+$lang['homepage']['servicos']['item1']['titulo'] = 'strategy and innovation';
+$lang['homepage']['servicos']['item1']['descricao'] = 'Business Development and Ideas <br>
+Market Strategy<br>
+Foreign Trade Consulting <br>
+Legal Consulting ';
+
+$lang['homepage']['servicos']['item2']['titulo'] = 'SUSTAINABILITY';
+$lang['homepage']['servicos']['item2']['descricao'] = 'Environmental Consultancy<br>
 Branding & Rebranding<br>
 UX/UI Design';
 
-$lang['homepage']['servicos']['item3']['titulo'] = 'Tecnologia';
-$lang['homepage']['servicos']['item3']['descricao'] = 'IOT (Internet Das Coisas)<br>
+$lang['homepage']['servicos']['item3']['titulo'] = 'tecnologia';
+$lang['homepage']['servicos']['item3']['descricao'] = 'IOT (Internet Of Things)<br>
 Machine Learning + Data Science (BI)<br>
 Engineering + IOT + Machine Learning';
 
-$lang['homepage']['contato']['h5'] = 'contactos';
-$lang['homepage']['contato']['h2'] = 'Contacte-nos';
+$lang['homepage']['contato']['h5'] = 'contacts';
+$lang['homepage']['contato']['h2'] = 'Contacts us';
 
 /** END HOMEPAGE **/
 
-/** SERVIÇOS **/
+/** Services **/
 
-$lang['servicos']['h5'] = 'SOBRE NÓS';
-$lang['servicos']['h1'] = 'O que fazemos?';
+$lang['servicos']['h5'] = 'ABOUT US';
+$lang['servicos']['h1'] = 'What we do?';
 
-$lang['servicos']['estrategia'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Estratégia e Inovação</h2>
+$lang['servicos']['estrategia'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Strategy and Innovation</h2>
 
-<strong class="upper we-500 color-orange pt-13">Desenvolvimento de Negócios e Ideias</strong>
+<strong class="upper we-500 color-orange pt-13">Business Development and Ideas</strong>
 <p class="pt-13 we-400">
-    Ao analisar o seu negócio, percebemos de que forma pode ter mais 
-    impacto no mercado, desenvolvendo novas ideias e objetivos para 
-    que haja um progresso mais fundamentado e bem sucedido.
+    When analyzing your business, we understand how you can have more
+    impact on the market, developing new ideas and objectives for
+    that there is more grounded and successful progress.
 </p>
 
-<strong class="upper we-500 color-orange pt-13">Estratégia de mercado</strong>
+<strong class="upper we-500 color-orange pt-13">Market strategy</strong>
 <p class="pt-13 we-400">
-    Desenvolvemos estratégias para alcançar o seu público alvo de forma 
-    mais direta e personalizada, baseadas na elaboração do seu perfil 
-    e do seu cliente.
+    We develop strategies to reach your target audience in a
+    more direct and personalized, based on the elaboration of your profile
+    and your client.
 </p>
 
-<strong class="upper we-500 color-orange pt-13">consultoria de comércio externo</strong>
+<strong class="upper we-500 color-orange pt-13">foreign trade consultancy</strong>
 <p class="pt-13 we-400">
-    Para que o seu negócio seja reconhecido internacionalmente, identificamos 
-    clientes que lhe permitam ampliar a rede de parceiros comerciais, de forma 
-    a solidificar a sua marca nesses países.
+    In order for your business to be recognized internationally, we identify
+    customers that allow it to expand the network of commercial partners, in a
+    to solidify your brand in those countries.
 </p>
 
-<strong class="upper we-500 color-orange pt-13">consultoria legal</strong>
+<strong class="upper we-500 color-orange pt-13">legal advice</strong>
 <p class="pt-13 we-400">
-    Fornecemos apoio e suporte legal, com a possibilidade de análise e melhoria dos critérios já existentes.
+    We provide support and legal support, with the possibility of analyzing and improving existing criteria.
 </p>';
 
-$lang['servicos']['sustentabilidade'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Sustentabilidade</h2>
+$lang['servicos']['sustentabilidade'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Sustainability </h2>
 
 <strong class="upper we-500 color-orange pt-13">
-    consultoria em meio ambiente
+    environmental consulting
 </strong>
 <p class="pt-13 we-400">
-    O impacto social e ambiental das empresas torna necessário que os modelos de 
-    negócio tradicionais façam uma migração que lhes permita estar em sintonia 
-    com princípios sustentáveis. 
+    The social and environmental impact of companies makes it necessary for models of
+    traditional businesses make a migration that allows them to be in tune
+    with sustainable principles.
     <br><br>
-    Na Consulting Brics temos a paixão de acompanhar a transição desse modelos de 
-    negócio, em especial na área do agronegócio. Para nós, ser sustentável significa 
-    aumentar os lucros usando menos recursos, com maior otimização e respeito pelos 
-    diretrizes ambientais.
+    At Consulting Brics, we are passionate about following the transition of these
+    business, especially in the area of ​​agribusiness. For us, being sustainable means
+    increase profits using fewer resources, with greater optimization and respect for
+    environmental guidelines.
 </p>
 
 <strong class="upper we-500 color-orange pt-13">
     branding & rebranding
 </strong>
 <p class="pt-13 we-400">
-    A personalidade do criador e a sua história são o ponto de partida para a análise 
-    que fazemos da sua marca. Criamos uma nova identidade visual ou aperfeiçoamos a 
-    já existente, com base num conjunto de informações.
+   The creator's personality and his story are the starting point for analysis
+    that we make of your brand. We create a new visual identity or improve the
+    already existing, based on a set of information.
     <br><br>
-    Em conjunto com os nossos profissionais de design corporativo, estudamos como a sua 
-    imagem empresarial deve ser projetada no mercado, tendo como objetivo a aquisição de 
-    um maior número de clientes, sobretudo em mercados sustentáveis.
+    Together with our corporate design professionals, we study how your
+    corporate image must be projected on the market, with the objective of acquiring
+    a greater number of customers, especially in sustainable markets.
     <br><br>
-    Mais especificamente, mostramos como o agronegócio tem a capacidade de impactar as 
-    causas ambientais e sustentáveis e o benefício que isso pode trazer à sua empresa 
-    e aos seus clientes.
+    More specifically, we show how agribusiness has the capacity to impact
+    environmental and sustainable causes and the benefit this can bring to your company
+    and your customers.
 </p>
 
 <strong class="upper we-500 color-orange pt-13">
     ux/ui design
 </strong>
 <p class="pt-13 we-400">
-    A nossa equipa de designers traduz os desejos dos nossos clientes numa comunicação 
-    visual personalizada para a sustentabilidade criativa. No agronegócio, esta é uma 
-    poderosa ferramenta para melhorar e aprimorar o seu desenvolvimento e reputação 
-    no mercado.
+    Our team of designers translates our customers' wishes into a communication
+    personalized look for creative sustainability. In agribusiness, this is a
+    powerful tool to improve and enhance your development and reputation
+    in the market.
 </p>';
 
-$lang['servicos']['tecnologia'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Tecnologia</h2>
+$lang['servicos']['tecnologia'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Technology</h2>
 
 <strong class="upper we-500 color-orange pt-13">
-    IOT (Internet Das Coisas)    
+    IOT (Internet Of Things)    
 </strong>
 <p class="pt-13 we-400">
 
-    Seguindo a procura crescente de tecnologias e inteligência na indústria 4.0, 
-    fornecemos aos nossos clientes uma nova forma de utilizar os dados gerados 
-    por dispositivos IoT. 
+    Following the growing demand for technologies and intelligence in industry 4.0,
+    we provide our customers with a new way to use the data generated
+    by IoT devices. 
     <br><br>
-    Assim, conseguimos reduzir custos e aumentar a produtividade, trazendo-lhe o poder 
-    de modernizar o seu negócio, com a facilidade de acompanhar todos os processos em 
-    tempo real, quase como se tivesse a sua empresa na palma da mão.
+    Thus, we managed to reduce costs and increase productivity, bringing you the power
+    to modernize your business, with the ease of monitoring all processes in
+    real time, almost as if you had your company in the palm of your hand.
 
 </p>
 
@@ -197,21 +197,21 @@ $lang['servicos']['tecnologia'] = '<h2 class="font-philosopher pt-28 color-blue 
     Machine Learning + Data Science (BI)
 </strong>
 <p class="pt-13 we-400">
-    Dispomos do conhecimento e capacidade para efetuar a análise de dados complexos, 
-    em comparação com dados meteorológicos de oxidação do solo e de nível pluviométrico.
+    We have the knowledge and ability to perform complex data analysis,
+    compared to meteorological data of soil oxidation and rainfall.
     <br><br>
-    Integrando sempre com os dados do seu negócio, traçamos uma trajetória cronológica, 
-    possibilitando melhorias e estimativas mais aproximadas para o futuro da sua empresa.
+    Always integrating with your business data, we trace a chronological trajectory,
+    enabling improvements and closer estimates for the future of your company.
 </p>
 
 <strong class="upper we-500 color-orange pt-13">
     Engineering + IOT + Machine Learning
 </strong>
 <p class="pt-13 we-400">
-    Temos a capacidade de criar, de forma única, dispositivos integrados com Machine Learning 
-    e Inteligência Artificial (IA). Através destas tecnologias, desenvolvemos projetos de 
-    implementação como uma fazenda inteligente ou um sistema de produção avançado, que 
-    trará vantagens sustentáveis e tecnológicas ao seu negócio. 
+    We have the ability to create, in a unique way, devices integrated with Machine Learning
+    and Artificial Intelligence (AI). Through these technologies, we develop projects of
+    implementation as a smart farm or an advanced production system, which
+    will bring sustainable and technological advantages to your business.
 </p>';
 
-/** END SERVIÇOS **/
+/** END Services **/
