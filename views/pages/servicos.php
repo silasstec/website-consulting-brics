b4f18bb4ee1ac96bcf3ca9b0ca78e88d4fd7730c
samuelrochac <samuelrocha.sys@gmail.com>
2021-01-19 13:08:46 -0300

    Configuração de tradução da pagina de serviços

--- views/pages/servicos.php
+++ views/pages/servicos.php
@@ -4,8 +4,8 @@
                 <div class="row">
                     <div class="col-12 md-center">
 
-                        <h5 class="color-orange we-300 pt-13 m-t-25">SOBRE NÓS</h5>
-                        <h1 class="color-blue font-philosopher pt-38 we-700 elipse-title-right m-b-25">O que fazemos?</h1>
+                        <h5 class="color-orange we-300 pt-13 m-t-25"><?php echo $lang['servicos']['h5']; ?></h5>
+                        <h1 class="color-blue font-philosopher pt-38 we-700 elipse-title-right m-b-25"><?php echo $lang['servicos']['h1']; ?></h1>
                         <div class="divider bg-blue m-b-50"></div>
                         <img src="assets/images/elipses-servicos.png" style="position: absolute; z-index:-1;right:-50px;margin-top:-180px;" class="d-none d-md-none d-lg-block d-xl-block">
                         <img class="elipses" src="assets/images/elipses-servicos-sm.png" >
@@ -15,33 +15,7 @@
 
                         <blockquote class="md-center"  data-aos="fade-right">
 
-                            <h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Estratégia e Inovação</h2>
-
-                            <strong class="upper we-500 color-orange pt-13">Desenvolvimento de Negócios e Ideias</strong>
-                            <p class="pt-13 we-400">
-                                Ao analisar o seu negócio, percebemos de que forma pode ter mais 
-                                impacto no mercado, desenvolvendo novas ideias e objetivos para 
-                                que haja um progresso mais fundamentado e bem sucedido.
-                            </p>
-
-                            <strong class="upper we-500 color-orange pt-13">Estratégia de mercado</strong>
-                            <p class="pt-13 we-400">
-                                Desenvolvemos estratégias para alcançar o seu público alvo de forma 
-                                mais direta e personalizada, baseadas na elaboração do seu perfil 
-                                e do seu cliente.
-                            </p>
-
-                            <strong class="upper we-500 color-orange pt-13">consultoria de comércio externo</strong>
-                            <p class="pt-13 we-400">
-                                Para que o seu negócio seja reconhecido internacionalmente, identificamos 
-                                clientes que lhe permitam ampliar a rede de parceiros comerciais, de forma 
-                                a solidificar a sua marca nesses países.
-                            </p>
-
-                            <strong class="upper we-500 color-orange pt-13">consultoria legal</strong>
-                            <p class="pt-13 we-400">
-                                Fornecemos apoio e suporte legal, com a possibilidade de análise e melhoria dos critérios já existentes.
-                            </p>
+                            <?php echo $lang['servicos']['estrategia']; ?>
 
                         </blockquote>
 
@@ -63,48 +37,7 @@
                     <div class="col-12 col-lg-10 col-xl-10">
                         <blockquote class="md-center"  data-aos="fade-left">
 
-                            <h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Sustentabilidade</h2>
-
-                            <strong class="upper we-500 color-orange pt-13">
-                                consultoria em meio ambiente
-                            </strong>
-                            <p class="pt-13 we-400">
-                                O impacto social e ambiental das empresas torna necessário que os modelos de 
-                                negócio tradicionais façam uma migração que lhes permita estar em sintonia 
-                                com princípios sustentáveis. 
-                                <br><br>
-                                Na Consulting Brics temos a paixão de acompanhar a transição desse modelos de 
-                                negócio, em especial na área do agronegócio. Para nós, ser sustentável significa 
-                                aumentar os lucros usando menos recursos, com maior otimização e respeito pelos 
-                                diretrizes ambientais.
-                            </p>
-
-                            <strong class="upper we-500 color-orange pt-13">
-                                branding & rebranding
-                            </strong>
-                            <p class="pt-13 we-400">
-                                A personalidade do criador e a sua história são o ponto de partida para a análise 
-                                que fazemos da sua marca. Criamos uma nova identidade visual ou aperfeiçoamos a 
-                                já existente, com base num conjunto de informações.
-                                <br><br>
-                                Em conjunto com os nossos profissionais de design corporativo, estudamos como a sua 
-                                imagem empresarial deve ser projetada no mercado, tendo como objetivo a aquisição de 
-                                um maior número de clientes, sobretudo em mercados sustentáveis.
-                                <br><br>
-                                Mais especificamente, mostramos como o agronegócio tem a capacidade de impactar as 
-                                causas ambientais e sustentáveis e o benefício que isso pode trazer à sua empresa 
-                                e aos seus clientes.
-                            </p>
-
-                            <strong class="upper we-500 color-orange pt-13">
-                                ux/ui design
-                            </strong>
-                            <p class="pt-13 we-400">
-                                A nossa equipa de designers traduz os desejos dos nossos clientes numa comunicação 
-                                visual personalizada para a sustentabilidade criativa. No agronegócio, esta é uma 
-                                poderosa ferramenta para melhorar e aprimorar o seu desenvolvimento e reputação 
-                                no mercado.
-                            </p>
+                            <?php echo $lang['servicos']['sustentabilidade']; ?>
 
                         </blockquote>
                     </div>
@@ -119,43 +52,7 @@
     
                         <blockquote class="md-center" data-aos="fade-right">
     
-                            <h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Tecnologia</h2>
-
-                            <strong class="upper we-500 color-orange pt-13">
-                                IOT (Internet Das Coisas)    
-                            </strong>
-                            <p class="pt-13 we-400">
-    
-                                Seguindo a procura crescente de tecnologias e inteligência na indústria 4.0, 
-                                fornecemos aos nossos clientes uma nova forma de utilizar os dados gerados 
-                                por dispositivos IoT. 
-                                <br><br>
-                                Assim, conseguimos reduzir custos e aumentar a produtividade, trazendo-lhe o poder 
-                                de modernizar o seu negócio, com a facilidade de acompanhar todos os processos em 
-                                tempo real, quase como se tivesse a sua empresa na palma da mão.
-    
-                            </p>
-    
-                            <strong class="upper we-500 color-orange pt-13">
-                                Machine Learning + Data Science (BI)
-                            </strong>
-                            <p class="pt-13 we-400">
-                                Dispomos do conhecimento e capacidade para efetuar a análise de dados complexos, 
-                                em comparação com dados meteorológicos de oxidação do solo e de nível pluviométrico.
-                                <br><br>
-                                Integrando sempre com os dados do seu negócio, traçamos uma trajetória cronológica, 
-                                possibilitando melhorias e estimativas mais aproximadas para o futuro da sua empresa.
-                            </p>
-    
-                            <strong class="upper we-500 color-orange pt-13">
-                                Engineering + IOT + Machine Learning
-                            </strong>
-                            <p class="pt-13 we-400">
-                                Temos a capacidade de criar, de forma única, dispositivos integrados com Machine Learning 
-                                e Inteligência Artificial (IA). Através destas tecnologias, desenvolvemos projetos de 
-                                implementação como uma fazenda inteligente ou um sistema de produção avançado, que 
-                                trará vantagens sustentáveis e tecnológicas ao seu negócio. 
-                            </p>
+                            <?php echo $lang['servicos']['tecnologia']; ?>
     
                         </blockquote>
 
