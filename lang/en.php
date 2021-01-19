6f7e8fb2cf80bf75fb4fad547937e1707671e3c4
samuelrochac <samuelrocha.sys@gmail.com>
2021-01-19 13:09:36 -0300

    Alterar o arquivo en.php com a tradução em ingles

---
+++ lang/en.php
@@ -0,0 +1,217 @@
+<?php 
+
+/** MENU**/
+
+$lang['menu']['sobre'] = 'SOBRE NÓS';
+$lang['menu']['contato'] = 'CONTACTO';
+$lang['menu']['servicos']['titulo'] = 'Serviços';
+$lang['menu']['servicos'][1] = 'ESTRATÉGIA E INOVAÇÃO';
+$lang['menu']['servicos'][2] = 'SUSTENTABILIDADE';
+$lang['menu']['servicos'][3] = 'TECNOLOGIA';
+
+/** END MENU **/
+
+/** FOOTER **/
+
+$lang['footer']['text'] = '2020 © Consulting Brics. Todos os direitos reservados. Developed By <a href="https://www.silastec.com" style="color:white; font-weight:900;">SilasTec</a>';
+$lang['footer']['legal'] = 'INFORMAÇÃO LEGAL';
+
+/** END FOOTER**/
+
+/** HOMEPAGE **/
+
+$lang['homepage'] = [];
+
+$lang['homepage']['header']['h1'] = 'Somos a ponte entre si e os seus objetivos.';
+$lang['homepage']['header']['button'] = 'CONTACTE-NOS';
+
+$lang['homepage']['sobre']['cargo_elenice'] = 'Fundadora & Chefe de Estratégia Internacional';
+$lang['homepage']['sobre']['cargo_antonio'] = 'Diretor Executivo (CEO)';
+
+$lang['homepage']['sobre']['h5'] = 'SOBRE NÓS';
+$lang['homepage']['sobre']['h2'] = 'Quem somos?';
+
+$lang['homepage']['sobre']['text1'] = "Somos uma consultora focada na migração sustentável.
+<br><br>
+No mundo empresarial, os modelos de negócio sustentável são um
+interesse crescente. É nesse sentido que proporcionamos métodos de
+apoio a empresas cujo objetivo passa por se renovarem de forma
+ecologicamente benéfica.";
+
+$lang['homepage']['sobre']['text2'] = "Aliado a isto, é conhecida a importância de uma empresa estar tão bem
+posicionada a nível nacional, como internacional. O crescimento fora do
+mercado brasileiro traz desafios diversos, nem sempre visíveis, que
+conhecemos e compreendemos.
+<br><br>
+Esta visão é, em grande parte, resultante da vasta experiência da nossa fundadora, 
+Elenice Candido da Silva, que se destaca como conhecedora e especialista nas 
+nossas áreas de atuação.
+<br><br>
+Com um fornecimento de serviços que ajuda os nossos clientes a expandirem-se 
+globalmente, exploramos desafios e conquistamos os seus objetivos.";
+
+$lang['homepage']['comentario'] = "A Consulting Brics nasceu do sonho de conectar o Brasil ao mundo e dar-lhe 
+a conhecer o seu enorme potencial. Nascida no Mato Grosso e criada na fazenda de família, idealizei e construí 
+uma empresa de consultoria global, com o objetivo de ajudar na urgente migração dos modelos de negócio para 
+as áreas da sustentabilidade.";
+
+
+$lang['homepage']['missao']['titulo'] = 'A nossa missão';
+
+$lang['homepage']['missao']['descricao'] = 'Suportar, através dos nossos serviços, a migração sustentável dos 
+modelos de negócio, para se adaptar às novas exigências do mercado 
+e valorizar o branding atribuindo um maior valor socioambiental.';
+
+$lang['homepage']['visao']['titulo'] = 'A nossa visão';
+
+$lang['homepage']['visao']['descricao'] = 'Criar um ambiente económico global onde empresas e particulares 
+podem potencializar ideias e negócios baseados nos pilares da 
+sustentabilidade.';
+
+
+$lang['homepage']['valores']['titulo'] = 'Os nossos valores';
+
+$lang['homepage']['valores']['descricao'] = 'Inovação na procura de soluções;
+Transparência com os clientes;
+Autenticidade na forma de atuar;
+Sofisticação perante os objetivos;
+Confiança nos nossos conhecimentos.';
+
+$lang['homepage']['servicos']['h5'] = 'Serviços';
+$lang['homepage']['servicos']['h2'] = 'O que fazemos?';
+$lang['homepage']['servicos']['item1']['titulo'] = 'estratégia e inovação';
+$lang['homepage']['servicos']['item1']['descricao'] = 'Desenvolvimento de Negócios e Ideias<br>
+Estratégia de Mercado<br>
+Consultoria em Comércio Externo<br>
+Consultoria Legal';
+
+$lang['homepage']['servicos']['item2']['titulo'] = 'SUSTENTABILIDADE';
+$lang['homepage']['servicos']['item2']['descricao'] = 'Consultoria em Meio Ambiente<br>
+Branding & Rebranding<br>
+UX/UI Design';
+
+$lang['homepage']['servicos']['item3']['titulo'] = 'Tecnologia';
+$lang['homepage']['servicos']['item3']['descricao'] = 'IOT (Internet Das Coisas)<br>
+Machine Learning + Data Science (BI)<br>
+Engineering + IOT + Machine Learning';
+
+$lang['homepage']['contato']['h5'] = 'contactos';
+$lang['homepage']['contato']['h2'] = 'Contacte-nos';
+
+/** END HOMEPAGE **/
+
+/** SERVIÇOS **/
+
+$lang['servicos']['h5'] = 'SOBRE NÓS';
+$lang['servicos']['h1'] = 'O que fazemos?';
+
+$lang['servicos']['estrategia'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Estratégia e Inovação</h2>
+
+<strong class="upper we-500 color-orange pt-13">Desenvolvimento de Negócios e Ideias</strong>
+<p class="pt-13 we-400">
+    Ao analisar o seu negócio, percebemos de que forma pode ter mais 
+    impacto no mercado, desenvolvendo novas ideias e objetivos para 
+    que haja um progresso mais fundamentado e bem sucedido.
+</p>
+
+<strong class="upper we-500 color-orange pt-13">Estratégia de mercado</strong>
+<p class="pt-13 we-400">
+    Desenvolvemos estratégias para alcançar o seu público alvo de forma 
+    mais direta e personalizada, baseadas na elaboração do seu perfil 
+    e do seu cliente.
+</p>
+
+<strong class="upper we-500 color-orange pt-13">consultoria de comércio externo</strong>
+<p class="pt-13 we-400">
+    Para que o seu negócio seja reconhecido internacionalmente, identificamos 
+    clientes que lhe permitam ampliar a rede de parceiros comerciais, de forma 
+    a solidificar a sua marca nesses países.
+</p>
+
+<strong class="upper we-500 color-orange pt-13">consultoria legal</strong>
+<p class="pt-13 we-400">
+    Fornecemos apoio e suporte legal, com a possibilidade de análise e melhoria dos critérios já existentes.
+</p>';
+
+$lang['servicos']['sustentabilidade'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Sustentabilidade</h2>
+
+<strong class="upper we-500 color-orange pt-13">
+    consultoria em meio ambiente
+</strong>
+<p class="pt-13 we-400">
+    O impacto social e ambiental das empresas torna necessário que os modelos de 
+    negócio tradicionais façam uma migração que lhes permita estar em sintonia 
+    com princípios sustentáveis. 
+    <br><br>
+    Na Consulting Brics temos a paixão de acompanhar a transição desse modelos de 
+    negócio, em especial na área do agronegócio. Para nós, ser sustentável significa 
+    aumentar os lucros usando menos recursos, com maior otimização e respeito pelos 
+    diretrizes ambientais.
+</p>
+
+<strong class="upper we-500 color-orange pt-13">
+    branding & rebranding
+</strong>
+<p class="pt-13 we-400">
+    A personalidade do criador e a sua história são o ponto de partida para a análise 
+    que fazemos da sua marca. Criamos uma nova identidade visual ou aperfeiçoamos a 
+    já existente, com base num conjunto de informações.
+    <br><br>
+    Em conjunto com os nossos profissionais de design corporativo, estudamos como a sua 
+    imagem empresarial deve ser projetada no mercado, tendo como objetivo a aquisição de 
+    um maior número de clientes, sobretudo em mercados sustentáveis.
+    <br><br>
+    Mais especificamente, mostramos como o agronegócio tem a capacidade de impactar as 
+    causas ambientais e sustentáveis e o benefício que isso pode trazer à sua empresa 
+    e aos seus clientes.
+</p>
+
+<strong class="upper we-500 color-orange pt-13">
+    ux/ui design
+</strong>
+<p class="pt-13 we-400">
+    A nossa equipa de designers traduz os desejos dos nossos clientes numa comunicação 
+    visual personalizada para a sustentabilidade criativa. No agronegócio, esta é uma 
+    poderosa ferramenta para melhorar e aprimorar o seu desenvolvimento e reputação 
+    no mercado.
+</p>';
+
+$lang['servicos']['tecnologia'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Tecnologia</h2>
+
+<strong class="upper we-500 color-orange pt-13">
+    IOT (Internet Das Coisas)    
+</strong>
+<p class="pt-13 we-400">
+
+    Seguindo a procura crescente de tecnologias e inteligência na indústria 4.0, 
+    fornecemos aos nossos clientes uma nova forma de utilizar os dados gerados 
+    por dispositivos IoT. 
+    <br><br>
+    Assim, conseguimos reduzir custos e aumentar a produtividade, trazendo-lhe o poder 
+    de modernizar o seu negócio, com a facilidade de acompanhar todos os processos em 
+    tempo real, quase como se tivesse a sua empresa na palma da mão.
+
+</p>
+
+<strong class="upper we-500 color-orange pt-13">
+    Machine Learning + Data Science (BI)
+</strong>
+<p class="pt-13 we-400">
+    Dispomos do conhecimento e capacidade para efetuar a análise de dados complexos, 
+    em comparação com dados meteorológicos de oxidação do solo e de nível pluviométrico.
+    <br><br>
+    Integrando sempre com os dados do seu negócio, traçamos uma trajetória cronológica, 
+    possibilitando melhorias e estimativas mais aproximadas para o futuro da sua empresa.
+</p>
+
+<strong class="upper we-500 color-orange pt-13">
+    Engineering + IOT + Machine Learning
+</strong>
+<p class="pt-13 we-400">
+    Temos a capacidade de criar, de forma única, dispositivos integrados com Machine Learning 
+    e Inteligência Artificial (IA). Através destas tecnologias, desenvolvemos projetos de 
+    implementação como uma fazenda inteligente ou um sistema de produção avançado, que 
+    trará vantagens sustentáveis e tecnológicas ao seu negócio. 
+</p>';
+
+/** END SERVIÇOS **/
