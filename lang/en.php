06f0db6b60bdd8a8b1cd77dc5523bf2331f160fc
silasstec <77503063+silasstec@users.noreply.github.com>
2021-01-20 19:41:47 +0000

    Update en.php

--- lang/en.php
+++ lang/en.php
@@ -34,9 +34,8 @@ $lang['homepage']['sobre']['h2'] = 'Who are we?';
 $lang['homepage']['sobre']['text1'] = "We are a consultant focused on sustainable migration.
 <br><br>
 In the business world, sustainable business models are a
-growing interest. It is in this sense that we provide methods of
-support to companies whose objective is to renew themselves in a
-ecologically beneficial.";
+growing interest. That's the reason why we provide methods of
+support to companies whose objective is an ecologically beneficial renewal.";
 
 $lang['homepage']['sobre']['text2'] = "In addition to this, the importance of a company being so well known
 positioned nationally, as internationally. Growth outside the
@@ -50,36 +49,35 @@ our areas of expertise.
 With a supply of services that helps our customers to expand
 globally, we explore challenges and achieve your goals.";
 
-$lang['homepage']['comentario'] = "Consulting Brics was born from the dream of connecting Brazil to the world and giving it
-to know its enormous potential. Born in Mato Grosso and raised on the family farm, I idealized and built
-a global consulting company, with the aim of helping in the urgent migration of business models to
-the areas of sustainability.";
+$lang['homepage']['comentario'] = "Consulting Brics was born from the dream of connecting Brazil
+to the world and giving it to know its enormous potential. Born in Mato Grosso and raised
+on the family farm, I idealized and built a global consulting company, intending to help in the urgent
+migration of business models to the areas of sustainability.";
 
-
-$lang['homepage']['missao']['titulo'] = 'Our mission';
+$lang['homepage']['missao']['titulo'] = 'Our Mission';
 
 $lang['homepage']['missao']['descricao'] = 'Support, through our services, the sustainable migration of
 business models, to adapt to new market demands
-and enhance branding by assigning greater socio-environmental value.';
+and enhance branding by assigning a greater socio-environmental value.';
 
-$lang['homepage']['visao']['titulo'] = 'Our vision';
+$lang['homepage']['visao']['titulo'] = 'Our Vision';
 
 $lang['homepage']['visao']['descricao'] = 'Create a global economic environment where companies and individuals
 can enhance ideas and businesses based on the pillars of
 sustainability.';
 
 
-$lang['homepage']['valores']['titulo'] = 'Our values';
+$lang['homepage']['valores']['titulo'] = 'Our Values';
 
 $lang['homepage']['valores']['descricao'] = 'Innovation in the search for solutions;
 Transparency with customers;
 Authenticity in the way of acting;
-Sophistication towards the objectives;
+Sophistication towards the goals;
 Trust in our knowledge. ';
 
 $lang['homepage']['servicos']['h5'] = 'Services';
 $lang['homepage']['servicos']['h2'] = 'What do we do?';
-$lang['homepage']['servicos']['item1']['titulo'] = 'strategy and innovation';
+$lang['homepage']['servicos']['item1']['titulo'] = 'Strategy and Innovation';
 $lang['homepage']['servicos']['item1']['descricao'] = 'Business Development and Ideas <br>
 Market Strategy<br>
 Foreign Trade Consulting <br>
@@ -90,12 +88,12 @@ $lang['homepage']['servicos']['item2']['descricao'] = 'Environmental Consultancy
 Branding & Rebranding<br>
 UX/UI Design';
 
-$lang['homepage']['servicos']['item3']['titulo'] = 'tecnologia';
+$lang['homepage']['servicos']['item3']['titulo'] = 'Technology';
 $lang['homepage']['servicos']['item3']['descricao'] = 'IOT (Internet Of Things)<br>
 Machine Learning + Data Science (BI)<br>
 Engineering + IOT + Machine Learning';
 
-$lang['homepage']['contato']['h5'] = 'contacts';
+$lang['homepage']['contato']['h5'] = 'Contacts';
 $lang['homepage']['contato']['h2'] = 'Contacts us';
 
 /** END HOMEPAGE **/
@@ -103,34 +101,32 @@ $lang['homepage']['contato']['h2'] = 'Contacts us';
 /** Services **/
 
 $lang['servicos']['h5'] = 'ABOUT US';
-$lang['servicos']['h1'] = 'What we do?';
+$lang['servicos']['h1'] = 'What do we do?';
 
 $lang['servicos']['estrategia'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Strategy and Innovation</h2>
 
 <strong class="upper we-500 color-orange pt-13">Business Development and Ideas</strong>
 <p class="pt-13 we-400">
-    When analyzing your business, we understand how you can have more
-    impact on the market, developing new ideas and objectives for
-    that there is more grounded and successful progress.
+    Our analysis of your business allows us to identify how you can have more impact
+on the market by developing new ideas and goals for more informed and successful progress.
 </p>
 
 <strong class="upper we-500 color-orange pt-13">Market strategy</strong>
 <p class="pt-13 we-400">
-    We develop strategies to reach your target audience in a
-    more direct and personalized, based on the elaboration of your profile
-    and your client.
+    We develop strategies to reach your target audience in a more direct and personalized way, based
+on the elaboration of your and your clients' profiles.
 </p>
 
 <strong class="upper we-500 color-orange pt-13">foreign trade consultancy</strong>
 <p class="pt-13 we-400">
-    In order for your business to be recognized internationally, we identify
-    customers that allow it to expand the network of commercial partners, in a
-    to solidify your brand in those countries.
+    For your business to be recognized internationally, we identify customers that allow it to expand
+the network of commercial partners in a way to solidify your brand in those countries. We develop strategies to
+reach your target audience in a more direct and personalized way, based on the elaboration of your and your clients' profiles.
 </p>
 
 <strong class="upper we-500 color-orange pt-13">legal advice</strong>
 <p class="pt-13 we-400">
-    We provide support and legal support, with the possibility of analyzing and improving existing criteria.
+    We provide support legal support, with the possibility of analyzing and improving existing criteria.
 </p>';
 
 $lang['servicos']['sustentabilidade'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Sustainability </h2>
@@ -139,41 +135,35 @@ $lang['servicos']['sustentabilidade'] = '<h2 class="font-philosopher pt-28 color
     environmental consulting
 </strong>
 <p class="pt-13 we-400">
-    The social and environmental impact of companies makes it necessary for models of
-    traditional businesses make a migration that allows them to be in tune
-    with sustainable principles.
+
+Companies' social and environmental impact makes it necessary for traditional business models to migrate in line with sustainable principles.
+
     <br><br>
-    At Consulting Brics, we are passionate about following the transition of these
-    business, especially in the area of ​​agribusiness. For us, being sustainable means
-    increase profits using fewer resources, with greater optimization and respect for
-    environmental guidelines.
+   At Consulting Brics, we are passionate about following these businesses transition, especially in the agribusiness area. 
+For us, being sustainable means increase profits using fewer resources, with greater optimization and respect for environmental guidelines.
 </p>
 
 <strong class="upper we-500 color-orange pt-13">
     branding & rebranding
 </strong>
 <p class="pt-13 we-400">
-   The creator\'s personality and his story are the starting point for analysis
+   The creator's personality and his story are the starting point for the analysis
     that we make of your brand. We create a new visual identity or improve the
     already existing, based on a set of information.
     <br><br>
-    Together with our corporate design professionals, we study how your
-    corporate image must be projected on the market, with the objective of acquiring
-    a greater number of customers, especially in sustainable markets.
+    Together with our corporate design professionals, we study how your corporate image must be projected on the market to acquire a 
+greater number of customers, especially in sustainable markets.
     <br><br>
-    More specifically, we show how agribusiness has the capacity to impact
-    environmental and sustainable causes and the benefit this can bring to your company
-    and your customers.
+    More specifically, we show how agribusiness has the capacity to impact environmental and sustainable causes and the
+benefit this can bring to your company and customers.
 </p>
 
 <strong class="upper we-500 color-orange pt-13">
     ux/ui design
 </strong>
 <p class="pt-13 we-400">
-    Our team of designers translates our customers\' wishes into a communication
-    personalized look for creative sustainability. In agribusiness, this is a
-    powerful tool to improve and enhance your development and reputation
-    in the market.
+Our team of designers translates our customers' wishes into a communication personalized look for creative sustainability. In agribusiness, this
+is a powerful tool to improve and enhance your development and reputation in the market.
 </p>';
 
 $lang['servicos']['tecnologia'] = '<h2 class="font-philosopher pt-28 color-blue we-700 m-b-50">Technology</h2>
@@ -184,12 +174,12 @@ $lang['servicos']['tecnologia'] = '<h2 class="font-philosopher pt-28 color-blue 
 <p class="pt-13 we-400">
 
     Following the growing demand for technologies and intelligence in industry 4.0,
-    we provide our customers with a new way to use the data generated
+    we provide our customers a new way to use the data generated
     by IoT devices. 
     <br><br>
-    Thus, we managed to reduce costs and increase productivity, bringing you the power
-    to modernize your business, with the ease of monitoring all processes in
-    real time, almost as if you had your company in the palm of your hand.
+
+    Thus, we managed to reduce costs and increase productivity, bringing you the power to modernize your business with the ease of monitoring
+all processes in real-time, almost as if you had your company in the palm of your hand.
 
 </p>
 
@@ -200,18 +190,17 @@ $lang['servicos']['tecnologia'] = '<h2 class="font-philosopher pt-28 color-blue 
     We have the knowledge and ability to perform complex data analysis,
     compared to meteorological data of soil oxidation and rainfall.
     <br><br>
-    Always integrating with your business data, we trace a chronological trajectory,
-    enabling improvements and closer estimates for the future of your company.
+    Through your business data, we trace a chronological trajectory that aims to allow improvements
+and more approximate estimates for the future of your company.
 </p>
 
 <strong class="upper we-500 color-orange pt-13">
     Engineering + IOT + Machine Learning
 </strong>
 <p class="pt-13 we-400">
-    We have the ability to create, in a unique way, devices integrated with Machine Learning
-    and Artificial Intelligence (AI). Through these technologies, we develop projects of
-    implementation as a smart farm or an advanced production system, which
-    will bring sustainable and technological advantages to your business.
+    We have the ability to create, in a unique way, devices integrated with Machine Learning and 
+Artificial Intelligence (AI). Using these technologies, we develop implementation projects like a 
+smart farm or an advanced production system, which will bring sustainable and technological advantages to your business.
 </p>';
 
 /** END Services **/
