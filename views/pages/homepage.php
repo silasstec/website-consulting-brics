5b90bac74068dceced2084e4ff3db54da583ff67
samuelrochac <samuelrocha.sys@gmail.com>
2021-01-26 09:10:56 -0300

    Correção de estilos

--- views/pages/homepage.php
+++ views/pages/homepage.php
@@ -342,7 +342,7 @@
                     </div>
 
                     <div>
-                        <img src="assets/images/elipses_contacto.png" class="elipses d-none d-md-block d-lg-block d-xs-block" style="margin-top:-100px;">
+                        <img src="assets/images/elipses_contacto.png" class="elipses d-none d-md-block d-lg-block d-xs-block">
                         <img src="assets/images/elipses_contactos_small.png" class="elipses d-block d-md-none d-lg-none d-xl-none" >
                         
                     </div>
