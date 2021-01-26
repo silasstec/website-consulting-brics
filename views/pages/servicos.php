7ce622a1b833b814ab0b4ff39b077b7521b23564
samuelrochac <samuelrocha.sys@gmail.com>
2021-01-26 10:11:59 -0300

    bolinhas animadas

--- views/pages/servicos.php
+++ views/pages/servicos.php
@@ -32,7 +32,24 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-2 col-xl-2 d-none d-lg-block d-xl-block">
-                        <img src="assets/images/elipses-titles.png" class="elipses d-none d-lg-block d-xl-block" style="margin-top:500px;">
+
+                        <svg style="margin-top:500px;" id="epigszj6rzg81" xmlns="http://www.w3.org/2000/svg" width="140px" height="120px" class="img-fluid elipses d-none d-lg-block d-xl-block" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 89 79" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
+                        <style>
+                            <![CDATA[#epigszj6rzg83_to {animation: epigszj6rzg83_to__to 3000ms linear 1s normal infinite}@keyframes epigszj6rzg83_to__to { 0% {offset-distance: 0%}
+                            33.333333% {offset-distance: 50%} 66.666667% {offset-distance: 100%} 100% {offset-distance: 100%} }#epigszj6rzg84_to 
+                            {animation: epigszj6rzg84_to__to 3000ms linear 1s normal infinite}@keyframes epigszj6rzg84_to__to { 0% {offset-distance: 0%} 33.333333% 
+                            {offset-distance: 50%} 66.666667% {offset-distance: 100%} 100% {offset-distance: 100%} }]]>
+                        </style>
+                        <circle id="epigszj6rzg82" r="25" transform="matrix(0.56964325147047 0 0 0.56964325147047 43.31890171000000 41.19280123000000)" 
+                            fill="rgb(253,134,61)" stroke="none" stroke-width="1"/>
+                        <g id="epigszj6rzg83_to" style="offset-path:path('M22.538910,58.540077C38.193472,77.581107,72.659089,60.701264,65.162410,50.347276C72.718109,60.469815,38.511066,77.600462,22.538910,58.972793');offset-rotate:0deg">
+                            <circle id="epigszj6rzg83" r="10" transform="scale(0.653891,0.653891) translate(0,0)" fill="rgb(249,98,5)" fill-opacity="0.5" 
+                            stroke="none" stroke-width="1"/>
+                        </g>
+                        <g id="epigszj6rzg84_to" style="offset-path:path('M13.538910,37.846692C29.972650,0.443842,74.101936,15.716554,67.777974,32.038327C73.924743,15.717673,29.908374,0.797524,14.099949,38.106194');offset-rotate:0deg">
+                            <circle id="epigszj6rzg84" r="14" transform="scale(0.653891,0.653891) translate(0,0)" fill="rgb(0,0,148)" fill-opacity="0.5" stroke="none" stroke-width="1"/>
+                        </g>
+                    </svg>
                     </div>
                     <div class="col-12 col-lg-10 col-xl-10">
                         <blockquote class="md-center"  data-aos="fade-left">
